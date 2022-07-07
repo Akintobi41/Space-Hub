c0a7bab206616f8ba3bda9f3d7ce32366f776ca9
Akintobi41 <akintobi41moyinoluwa@gmail.com>
2022-07-07 23:38:19 +0100

    updated JS Files

---
+++ Index.js
@@ -0,0 +1,18 @@
+let menuToggle = document.querySelector(".menuToggle");
+      let navigation = document.querySelector(".navigation");
+
+      document.onclick = function (e) {
+        if (
+          e.target != document.querySelector(".hamburger") &&
+          e.target != document.querySelector(".icon-close")
+        ) {
+          menuToggle.classList.remove("active");
+          navigation.classList.remove("active");
+        }
+      };
+
+      menuToggle.onclick = function () {
+        menuToggle.classList.toggle("active");
+        navigation.classList.toggle("active");
+      };
+    </script>
