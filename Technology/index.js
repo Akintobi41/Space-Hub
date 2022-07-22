ec18876ebd7d0057d1f0dd3222752e3762993f28
Akintobi41 <akintobi41moyinoluwa@gmail.com>
2022-07-22 10:36:42 +0100

    updated styles

--- Technology/index.js
+++ Technology/index.js
@@ -128,7 +128,7 @@ function newImage(imgEdit) {
 
                     techTitle.innerHTML = "SPACE CAPSULE";
                     techText.innerHTML =
-                        "Space capsule A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.";
+                        "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.";
                 }
 
                 if (e.target.className.endsWith("03")) {
